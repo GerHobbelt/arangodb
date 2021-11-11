ac4b71145e4c9d0ddddbf9c3e43ef640420759ea
Vadim Kondratyev <vadim@arangodb.com>
2021-11-11 22:24:03 +0100

    arangodb release 3.8.3

--- js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
+++ js/apps/system/_admin/aardvark/APP/react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.fac3a0f1a3e3b48a94c8efd5544566ec.js"
+  "precache-manifest.2bcb80558f48baa13313c9208a825ad3.js"
 );
 
 workbox.clientsClaim();
